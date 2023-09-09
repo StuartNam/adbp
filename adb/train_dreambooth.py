ec866a81bd545f8ded76f1e45ea8b50cf6e92dc0
Nam Ha <tokarinrika@gmail.com>
2023-09-09 10:25:57 +0700

    Update

--- adb/train_dreambooth.py
+++ adb/train_dreambooth.py
@@ -433,7 +433,9 @@ class DreamBoothDataset(Dataset):
         if not self.instance_data_root.exists():
             raise ValueError(f"Instance {self.instance_data_root} images root doesn't exists.")
 
+
         self.instance_images_path = list(Path(instance_data_root).iterdir())
+        print(self.instance_images_path)
         self.num_instance_images = len(self.instance_images_path)
         self.instance_prompt = instance_prompt
         self._length = self.num_instance_images
