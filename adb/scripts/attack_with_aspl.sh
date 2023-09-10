8cc5998210224871a57cd2e8e35e26a2451c1f3d
First Last <tokarinrika@gmail.com>
2023-09-10 14:46:42 +0000

    Update

--- adb/scripts/attack_with_aspl.sh
+++ adb/scripts/attack_with_aspl.sh
@@ -1,44 +1,44 @@
 export EXPERIMENT_NAME="ASPL"
 export MODEL_PATH="stabilityai/stable-diffusion-2-1-base"
-export CLEAN_TRAIN_DIR="data/n000050/set_A" 
-export CLEAN_ADV_DIR="data/n000050/set_B"
-export OUTPUT_DIR="outputs/$EXPERIMENT_NAME/n000050_ADVERSARIAL"
-export CLASS_DIR="data/class-person"
+export CLEAN_TRAIN_DIR="db_dataset/5/set_A" 
+export CLEAN_ADV_DIR="db_dataset/5/set_B"
+export OUTPUT_DIR="outputs/$EXPERIMENT_NAME/5_ADVERSARIAL"
+export CLASS_DIR="db_dataset/class-person"
 
 
-# # ------------------------- Train ASPL on set B -------------------------
-# mkdir -p $OUTPUT_DIR
-# cp -r $CLEAN_TRAIN_DIR $OUTPUT_DIR/image_clean
-# cp -r $CLEAN_ADV_DIR $OUTPUT_DIR/image_before_addding_noise
+# ------------------------- Train ASPL on set B -------------------------
+mkdir -p $OUTPUT_DIR
+cp -r $CLEAN_TRAIN_DIR $OUTPUT_DIR/image_clean
+cp -r $CLEAN_ADV_DIR $OUTPUT_DIR/image_before_addding_noise
 
-# accelerate launch --main_process_port 29051 attacks/aspl.py \
-#   --pretrained_model_name_or_path=$MODEL_PATH  \
-#   --enable_xformers_memory_efficient_attention \
-#   --instance_data_dir_for_train=$CLEAN_TRAIN_DIR \
-#   --instance_data_dir_for_adversarial=$CLEAN_ADV_DIR \
-#   --instance_prompt="a photo of sks person" \
-#   --class_data_dir=$CLASS_DIR \
-#   --num_class_images=200 \
-#   --class_prompt="a photo of person" \
-#   --output_dir=$OUTPUT_DIR \
-#   --center_crop \
-#   --with_prior_preservation \
-#   --prior_loss_weight=1.0 \
-#   --resolution=512 \
-#   --train_batch_size=1 \
-#   --max_train_steps=50 \
-#   --max_f_train_steps=3 \
-#   --max_adv_train_steps=6 \
-#   --checkpointing_iterations=10 \
-#   --learning_rate=5e-7 \
-#   --pgd_alpha=5e-3 \
-#   --pgd_eps=5e-2
-#   # --train_text_encoder \
+accelerate launch --main_process_port 29051 attacks/aspl.py \
+  --pretrained_model_name_or_path=$MODEL_PATH  \
+  --enable_xformers_memory_efficient_attention \
+  --instance_data_dir_for_train=$CLEAN_TRAIN_DIR \
+  --instance_data_dir_for_adversarial=$CLEAN_ADV_DIR \
+  --instance_prompt="a photo of sks person" \
+  --class_data_dir=$CLASS_DIR \
+  --num_class_images=200 \
+  --class_prompt="a photo of person" \
+  --output_dir=$OUTPUT_DIR \
+  --center_crop \
+  --with_prior_preservation \
+  --prior_loss_weight=1.0 \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --max_train_steps=50 \
+  --max_f_train_steps=3 \
+  --max_adv_train_steps=6 \
+  --checkpointing_iterations=10 \
+  --learning_rate=5e-7 \
+  --pgd_alpha=5e-3 \
+  --pgd_eps=5e-2 \
+  --train_text_encoder \
 
 
 # ------------------------- Train DreamBooth on perturbed examples -------------------------
 export INSTANCE_DIR="$OUTPUT_DIR/noise-ckpt/50"
-export DREAMBOOTH_OUTPUT_DIR="outputs/$EXPERIMENT_NAME/n000050_DREAMBOOTH"
+export DREAMBOOTH_OUTPUT_DIR="outputs/$EXPERIMENT_NAME/5_DREAMBOOTH"
 
 accelerate launch train_dreambooth.py \
   --pretrained_model_name_or_path=$MODEL_PATH  \
@@ -64,4 +64,4 @@ accelerate launch train_dreambooth.py \
   --mixed_precision=fp16 \
   --prior_generation_precision=fp16 \
   --sample_batch_size=8 \
-  # --train_text_encoder \
+  --train_text_encoder \
--- adb/scripts/train_dreambooth_alone.sh
+++ adb/scripts/train_dreambooth_alone.sh
@@ -1,7 +1,7 @@
 export MODEL_PATH="stabilityai/stable-diffusion-2-1-base"
-export INSTANCE_DIR="data/n000050/set_A/"
-export CLASS_DIR="data/class-person"
-export DREAMBOOTH_OUTPUT_DIR="dreambooth-outputs/n000050/"
+export INSTANCE_DIR="db_dataset/5/set_A/"
+export CLASS_DIR="db_dataset/class-person"
+export DREAMBOOTH_OUTPUT_DIR="dreambooth-outputs/5/"
 
 accelerate launch train_dreambooth.py \
   --pretrained_model_name_or_path=$MODEL_PATH  \
@@ -14,7 +14,7 @@ accelerate launch train_dreambooth.py \
   --prior_loss_weight=1.0 \
   --instance_prompt="a photo of sks person" \
   --class_prompt="a photo of person" \
-  --inference_prompt="a photo of sks person;a DSLR portrait of sks person" \
+  --inference_prompt="sks person sitting on a chair;sks person behind the bars" \
   --resolution=512 \
   --train_batch_size=2 \
   --gradient_accumulation_steps=1 \
